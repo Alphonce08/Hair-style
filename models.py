e16d06403d2d317378aaa41994b21790a02d4c58
Alphonce <alphoncerotich30@gmail.com>
2023-09-16 12:21:11 +0300

    new update cut

---
+++ models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TimeStamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    
+    class Meta:
+        abstract = True
+       
+
+class Category(models.Model):
+    name= models.CharField(max_length=30, blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+    
+    def __str__(self):
+        return str(self.name)
+        
+class Images(models.Model):
+    ''' Images class to hold general images to be used anywhere in the product'''
+    images = models.ImageField(upload_to="media")
+    titr = models.CharField(max_length=200)
+    create = models.DateField(auto_now_add=True)
+    update = models.DateField(auto_now=True)
+
+    class Meta:
+        def __str__(self):
+            return self(self.titr)
+            
+            
+class Product(TimeStamp):#inherit TimeStamp class
+    '''This represent an instance of a product to be sold in the market/platform'''
+    
+    name= models.CharField(max_length=30, blank=False, null=False)
+    price = models.FloatField(blank=False, null=False)
+    image =  models.ImageField(upload_to="images", blank=True, null=True)#image to be displayed for the product
+    description = models.TextField(max_length=220, blank=True)
+    
+    sold  = models.BooleanField(default=False, blank=True, null=True)
+    
+    other_image = models.ForeignKey(Images,on_delete=models.CASCADE,blank=True, null=True)#other images that can be displayed along the main image
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,blank=True, null=True)
+  
+
+    def __str__(self):
+        return str(self.name)
+    class Meta:
+        get_latest_by= "id"
+
+
+class Shopping(TimeStamp):#inherit TimeStamp class
+    '''
+    Create an instance of shopping by a buyer
+    A single order can have many products and can be assosiated to aa registered user
+    '''
+
+    user =models.ForeignKey(User,on_delete=models.CASCADE,blank=True, null=True)
+  
+  
+    def __str__(self):
+        return str(self.id)    
+        
+class Order(TimeStamp):#inherit TimeStamp class
+    '''
+    Create an instance of order by a buyer
+    A single order can have many order
+    '''
+
+    shopping = models.ForeignKey(Shopping,on_delete=models.CASCADE,blank=True, null=True)
+
+    def __str__(self):
+        return str(self.shopping)   
---
+++ views.py
@@ -0,0 +1,103 @@
+from django.shortcuts import render, redirect
+from .models import Fashion
+import os
+from django.conf import settings
+from .models import Images,Product
+
+     
+
+#image upload
+def img(request):
+    images = Images.objects.all()
+    context = {"images": images}
+    return render(request, "electronic.html", "fashion.html", "jewellery.html", context)       
+        
+
+
+
+#def home(request):
+    #e_prod= Product.objects.filter(category_id=1,sold=False)#.last
+    #f_prod= Product.objects.filter(category_id=6,sold=False)
+    #j_prod= Product.objects.filter(category_id=2,sold=False)
+
+
+  
+    # if len(list(e_prod)) >= 3:
+    #     last3 = len(list(e_prod))-3
+    # else:
+    #     last3 = len(list(e_prod))
+
+    # e_prod3=list(e_prod)[last3:]
+    
+    # print(e_prod)
+    
+    
+    #context = {"e_prod":e_prod ,"f_prod":f_prod,"j_prod":j_prod,}
+
+   # return render(request, 'index.html', context)
+    
+
+
+def electronic(request):
+    e_prod= Product.objects.filter(category_id=1,sold=False)
+    context = {"e_prod":e_prod }
+ 
+    return render(request, 'electronic.html', context)
+
+def fashion(request):
+    f_prod= Product.objects.filter(category_id=6,sold=False)
+    context = {"f_prod":f_prod }
+    return render(request, 'fashion.html', context)
+   
+
+def jewellery(request):
+    j_prod= Product.objects.filter(category_id=2,sold=False)
+    context = {"j_prod":j_prod }
+   
+    return render(request, 'jewellery.html', context)
+    
+    
+  
+def readmore(request):
+    return render(request, 'readmore.html')
+    
+def buy(request):
+    return render(request, 'buy.html')    
+def more(request):
+    m_prod= Product.objects.filter(category_id=1,sold=False)
+    context = {"m_prod":m_prod }
+ 
+    return render(request, 'more.html')   
+    
+    #contact page
+def contact(request):
+     success_massage=""
+
+     if request.method == "POST":
+        phonenumber = request.POST.get('phonenumber')
+        delivery = request.POST.get('delivery')
+        category = request.POST.get('category')
+       
+        query = Fashion(phonenumber=phonenumber, delivery=delivery, category=category)
+        query.save()
+        success_massage = "You have sucessfuly send your infomation, You will get within 3 days of your request. Thanks for ordering"
+        return render(request, 'contact.html',{"mmg1":success_massage,"mmg2":777})
+     return render(request, 'contact.html',{"mmg1":success_massage,"mmg2":777})
+
+   #payment 
+def oauth_success(request):
+    r = cl.access_token()
+    return JsonResponse(r, safe=False)
+
+
+def lipamimi(request):
+    if request.method == "POST":
+        phone_number = request.POST.get('phone')
+        amount = request.POST.get('amount')
+        amount = int(amount)
+        account_reference = 'WANYAMA'
+        transaction_desc = 'STK Push Description'
+        callback_url = stk_push_callback_url
+        r = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
+        return JsonResponse(r.response_description, safe=False)
+    return render(request, 'buy.html')
